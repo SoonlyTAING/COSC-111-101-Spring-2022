bb87e4f6a3f8c005ee629df10739ae8f4b830942
ChhaySereyVath <102718145+ChhaySereyVath@users.noreply.github.com>
2022-04-02 23:22:47 +0700

    Update Ex 2.py

--- Ex 2.py
+++ Ex 2.py
@@ -8,3 +8,13 @@ def index_power(array,index):
     x=array[index]**index
     return x
 print(index_power([1, 2, 3, 4], 3))
+
+def index_power(array, index):
+    if index < len(array):
+        res = array[index] ** index
+    else:
+        res = -1
+    return res
+
+
+print(index_power([1, 3, 10, 100], 3))
